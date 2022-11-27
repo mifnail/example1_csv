be7f2b2aff8da23cbbbe846c41e67f910954b75a
ilias <mifnail@yandex.ru>
2022-11-27 20:45:31 +0400

    Адаптируем под студентов

--- main.py
+++ main.py
@@ -1,35 +1,73 @@
-from emp_csv import *
-FILENAME= 'data.csv'
-MENU = {
-    '1':'Открыть файл',
-    '2':'Добавить',
-    '3':'Улдалить',
-    '4':'Поиск',
-    '5':'Средний возраст',
-    '6':'Сохранить',
-    '7':'',
-    '0': '<-Меню',
-    'exit':'Выход'
-}
-for k,v in MENU.items():
-    print(k,'-',v)
+import csv
+csv_file =[]
+#Открыть файл
+def file_open():
+    global csv_file
+    with open('data.csv', 'r',newline='',encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        for row in reader:
+            csv_file.append(row)
+        print('Файл открыт. Записей:', len(csv_file))
+#Добавление
+def insert(fio,age,tel,grp,pol,email,kurs):
+    global csv_file
+    try:
+        mx=max(csv_file,key=lambda x:int(x['ном']))
+        csv_file.append({'ном':int(mx['ном'])+1,
+                         'фио':fio,
+                         'возраст':age,
+                         'телефон':tel,
+                         'группа':grp,
+                         'пол':pol,
+                         'почта':email,
+                         'курс':kurs
+                         })
+    except Exception as e:
+        return f' Ошибка при добавлении записи{e}'
+    print('Записи добавлены')
 
-while True:
-    action =input('>_')
-    if action=='1':
-        file_open()
-    elif action=='2':
-        insert(input('ФИО:'),input('Возраст:'),input('Телефон:'),input('Отдел:'))
-    elif action == '3':
-        print(drop_by_arg(input("Значение"), input("Колонка")))
-    elif action == '4':
-        find(input("Значение"), input("Колонка"))
-    elif action=='6':
-        save(FILENAME)
-    elif action=='7':
-        show_rows()
-    elif action == '0':
-        for key, val in MENU.items():
-            print(key, '-', val)
-    elif action == 'exit':
-        break
+#Удаление
+def drop_by_arg(val,col_name='фио'):
+    global csv_file
+    try:
+        csv_file = list(filter(lambda x:x[col_name] != val), csv_file)
+    except Exception as e:
+        return f'Строка с значением{val}поля {col_name} не найдена'
+    return (f'Строка с значением{val}поля {col_name} удалена')
+
+#Поиск
+def find(val,col_name='фио'):
+    print(*list(filter(lambda x: x[col_name]==val,csv_file)))
+
+#Средний возраст
+def avg_age():
+    print("Средний возраст",sum([int(row['возраст'] for row in csv_file)]) // len(csv_file))
+
+
+
+
+#Сохранить
+def save(file_name):
+        with open(file_name,'w',encoding='utf-8',newline='') as file:
+            columns =['ном',"фио","возраст","телефон","группа",'пол','почта','курс']
+            writer=csv.DictWriter(file, delimiter=";",fieldnames=columns)
+            writer.writeheader()
+            writer.writerows(csv_file)
+            print('Файлы сохранены')
+
+def show_rows():
+    if len(csv_file) == 0:
+        print(type(csv_file))
+    else:
+        print('{:<5}{:<25}{:<8}{:<12}{:<15}'.format(
+            'ном', 'фио', 'возраст', 'телефон', 'группа','пол','почта','курс'
+        ))
+        for el in csv_file:
+            print('{:<5}{:<25}{:<8}{:<12}{:<15}{:<15}{:<15}{:<15}'.format(el["ном"],
+                el["фио"],
+                el["возраст"],
+                el["телефон"],
+                el['группа'],
+                el['пол'],
+                el['почта'],
+                el['курс']))
